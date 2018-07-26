39ebb5123b9f66824d673ef17ebe320a19085ced
Jakub Tomeš (jtomes123) <jtomes123@gmail.com>
2018-07-26 21:25:02 +0200

    Setup of routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Main',
+    component: Main
+  }]
 })
