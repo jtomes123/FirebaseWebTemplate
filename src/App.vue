dff61f343b86979f26eab258cef5ebffd09904cd
Jakub Tomeš (jtomes123) <jtomes123@gmail.com>
2018-07-26 21:24:41 +0200

    Minor edits

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -13,11 +12,19 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: rgba(110, 111, 110, 1);
+}
+.flex-centered-horizontal {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+html {
+  background-color: rgba(14, 14, 14, 1);
 }
 </style>
