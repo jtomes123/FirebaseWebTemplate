51fe2bc5b076a0312069661cc535b733626e31cc
Jakub Tomeš (jtomes123) <jtomes123@gmail.com>
2018-07-26 21:25:28 +0200

    Importing components, setup of firebase

--- src/main.js
+++ src/main.js
@@ -3,13 +3,46 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+import Projects from './components/Projects.vue'
+import About from './components/About.vue'
+import Contact from './components/Contact.vue'
+import Icon from 'vue-awesome'
 
 Vue.config.productionTip = false
+
+// Register custom components
+Vue.component('p-projects', Projects)
+Vue.component('p-about', About)
+Vue.component('p-contact', Contact)
+Vue.component('icon', Icon)
+
+// Use the BootstrapVue and VueFire extension
+Vue.use(VueFire)
+Vue.use(BootstrapVue)
+
+// Here, you will enter your firebase credentials
+firebase.initializeApp({
+  projectId: 'YOUR PROJECT ID HERE',
+  databaseURL: 'YOUR FIREBASE DATABASE URL HERE'
+})
+
+export const db = firebase.firestore()
+db.settings({
+  timestampsInSnapshots: true
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
